1123f1c1c3595be60d2cacf04477a5cf30ca9996
Retzam Tarle <Younggregs>
2019-10-31 01:42:37 +0100

    push push

--- src/store/index.js
+++ src/store/index.js
@@ -6,6 +6,7 @@ import { composeWithDevTools } from 'redux-devtools-extension/'
 import createSagaMiddleware from 'redux-saga'
 import rootSaga from '../saga'
 
+
 const sagaMiddleware = createSagaMiddleware()
 const store = createStore(
                         rootReducer, 
